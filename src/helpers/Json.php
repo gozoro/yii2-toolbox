5ee046ffe87569c03c0cc7edba686f278698266c
gozoro <gozoro@yandex.ru>
2024-04-03 19:19:38 +0300

    Use package gozoro\yii2-helpers

--- src/helpers/Json.php
+++ src/helpers/Json.php
@@ -2,41 +2,12 @@
 
 namespace gozoro\toolbox\helpers;
 
-use yii\helpers\ArrayHelper;
+
 
 /**
  * Json helper
  */
-class Json extends \yii\helpers\Json
+class Json extends \gozoro\yii2\helpers\Json
 {
-
-	/**
-	 * Encodes the given options into a JSON string.
-	 *
-	 * @param array $options
-	 * @return string
-	 */
-	public static function optionsEncode(array $options)
-	{
-		$jsOptions = [];
-
-		foreach($options as $key => $val)
-		{
-			if(is_string($val) and strpos($val, 'function')===0)
-			{
-				$jsOptions[$key] = $key.':'.$val;
-			}
-			elseif(ArrayHelper::isAssociative($val))
-			{
-				$jsOptions[$key] = $key.':'.static::optionsEncode($val);
-			}
-			else
-			{
-				$jsOptions[$key] = $key.':'.static::encode($val);
-			}
-		}
-
-
-		return '{'.implode(',', $jsOptions).'}';
-	}
+	// For backward compatibility
 }
